42994e3bb03912f89d694b897f5f20a640955885
danielrepos <79573960+danielrepos@users.noreply.github.com>
2024-05-05 17:43:47 +0530

    Build fix #3: removed childManagement_backup

--- app/childManagement_backup/components/graduateModalValidation.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const GraduationModalValidationSchema = yup.object().shape({
-  graduationDate: yup.string().required("Graduation Date is required"),
-});
-
-export const AssignClassroomValidationSchema = yup.object().shape({
-  classroom: yup.string().required("Classroom is required"),
-});
